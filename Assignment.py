c247417743de89bf63bed7e32340d7e8aa6fd46e
RachMcDonnell <101214502+RachMcDonnell@users.noreply.github.com>
2022-05-03 20:56:28 +0100

    Update Assignment.py
    Added docstrings to beginning of script to explain the focus of the assignment topic.

--- Assignment.py
+++ Assignment.py
@@ -1,17 +1,28 @@
-# GIS Practical 2 Script.
+"""Assignment script using GIS Practical 2 from module EGM711.
+
+ Practical 2 aims to focus on displaying. collating, creating and querying data in ArcGIS.
+ Topics include:
+ Adding, symbolising and navigating spatial data
+ Clipping large datasets to an area of interest
+ Accessing linked attribute data
+ Creating a points shapefile from tabular data
+ Accessing and selecting attribute and spatial data through attribute and locational queries
+ Creating a Digital Terrain Model"""
 
 # Import modules required for the practical.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#
 import pandas as pd
 import geopandas as gpd
- from shapely.geometry import Point, LineString, Polygon
+from shapely.geometry import Point, LineString, Polygon
 import matplotlib.pyplot as plt
- from cartopy.feature import ShapelyFeature
+from cartopy.feature import ShapelyFeature
 import cartopy.crs as ccrs
 import matplotlib.patches as mpatches
 import matplotlib.lines as mlines
 import rasterio as rio
-
+import numpy as np
+#
+        
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
