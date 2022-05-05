79aca61edbf2ca7ab5117e3d4828a32bdb1079c0
RachMcDonnell <101214502+RachMcDonnell@users.noreply.github.com>
2022-05-05 22:44:12 +0100

    Update assignment.py-Layout of script
    -Add 'study area' shapefile .
    -Create draft layout of remainder of script using embedded comments as a guide for each section.
    -Begin developing a function to clip all layers to study area box
    -Begin to plot data to map

--- Assignment.py
+++ Assignment.py
@@ -22,21 +22,67 @@ import matplotlib.lines as mlines
 import rasterio as rio
 import numpy as np
 
-# Exercise 1: Load practical data and adding and symbolising data
+# Exercise 1: Copying practical data and adding and symbolising data
+
+# Download Practical 2 data from the data files folder and load data here:
 outline = gpd.read_file('data_files/NI_outline.shp')
 roads = gpd.read_file('data_files/NI_roads.shp')
 settlements_poly = gpd.read_file('data_files/settlements_poly.shp')
 gazeteer = gpd.read_file('data_files/Gazeteer.shp')
-binvenagh_250k = rio.open('data_files/Binevenagh_250k.tif')
+binevenagh_250k = rio.open('data_files/Binevenagh_250k.tif')
+
+# Add NI_Outline and NI_Roads layers to current map:
+# create a figure of size 10x10 (representing the page size in inches)
+myFig = plt.figure(figsize=(10, 10))
+
+myCRS = ccrs.UTM(29)  # create a Universal Transverse Mercator reference system to transform our data.
+
+ax = plt.axes(projection=ccrs.Mercator())  # finally, create an axes object in the figure, using a Mercator
+# projection, where we can actually plot our data.
+
+#  add the outline of Northern Ireland using cartopy's ShapelyFeature
+outline_feature = ShapelyFeature(outline['geometry'], myCRS, edgecolor='k', facecolor='w')
+
+xmin, ymin, xmax, ymax = outline.total_bounds
+ax.add_feature(outline_feature)  # add the features we've created to the map.
+
+#  add the Northern Ireland Roads using cartopy's ShapelyFeature
+outline_feature = ShapelyFeature(outline['geometry'], myCRS, edgecolor='k', facecolor='w')
 
 
-def print_hi(name):
+def clip_to_study_area(name):
     # Use a breakpoint in the code line below to debug your script.
+
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
+"""Clips layers to the Study Area.
+
+ Each layer must be clipped to the 'Study Area' Shapefile"""
+
+# Exercise 2 Clipping Map Layers:
+study_area = gdp.readfile('data_files/study_area_box.shp')  # Add map layer called Study Area Box
+
+# Check both layers are the same CRS
+assert isinstance(roads.crs, object)
+print(study_area.crs == roads.crs)
+# Clip roads layer to the Study Area Box
+roads_clip = gdp.clip(roads, study_area)
+
 if __name__ == '__main__':
     print_hi('PyCharm')
+# Exercise 3 Creating and applying a layer file
+# Exercise 4 Obtaining Map layers online and clipping them to the study area
+# Exercise 5 Symbolising Quantities
+# Exercise 6 Creating and displaying point data and saving to a shapefile (Track Centre)
+# Exercise 7 Displaying point data and saving to a shapefile (Buildings)
+# Exercise 8 Selecting Features from within the attribute table
+# Exercise 9 Selecting Features from within the map
+# Exercise 10 Selecting Features by attribute
+# Exercise 11 Selecting Features by location
+# Exercise 12 Impact of scale on accuracy and detail
+# Exercise 13 Raster and the impact of resolution (DTM)
+# Exercise 14 Create a Map Layout
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
