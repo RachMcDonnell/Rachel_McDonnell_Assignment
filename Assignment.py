b97090c9aef6d4ec28c5bad965b0186c435d9303
RachMcDonnell <101214502+RachMcDonnell@users.noreply.github.com>
2022-05-15 22:14:26 +0100

    Update Assignment.py
    Re- order code so that map extent is associated with the study area.
    Update colours of road classes for better display.
    Update facecolor of roads to 'none'.

--- Assignment.py
+++ Assignment.py
@@ -21,7 +21,7 @@ import matplotlib.lines as mlines
 import rasterio as rio
 import numpy as np
 
-# Exercise 1: Copying practical data and adding and symbolising data
+# Part 1: Copying practical data and adding and symbolising data
 
 # Download Practical 2 data from the data files folder and load data here:
 outline = gpd.read_file('data_files/NI_outline.shp')
@@ -29,8 +29,10 @@ roads = gpd.read_file('data_files/NI_roads.shp')
 settlements_poly = gpd.read_file('data_files/settlements_poly.shp')
 gazeteer = gpd.read_file('data_files/Gazeteer.shp')
 binevenagh_250k = rio.open('data_files/Binevenagh_250k.tif')
+study_area = gpd.read_file('data_files/study_area_box.shp')
 
-# Add NI_Outline and Road layers to current map:
+# Part 2 :Add Layers to current map and updating symbology:
+
 # create a figure of size 10x10 (representing the page size in inches)
 myFig = plt.figure(figsize=(10, 10))
 
@@ -39,11 +41,14 @@ myCRS = ccrs.UTM(29)  # create a Universal Transverse Mercator reference system 
 ax = plt.axes(projection=ccrs.Mercator())  # finally, create an axes object in the figure, using a Mercator
 # projection, where we can actually plot our data.
 
-#  add the outline of Northern Ireland using cartopy's ShapelyFeature
+#  add the outline of Northern Ireland using cartopy's ShapelyFeature with a black outline
 outline_feature = ShapelyFeature(outline['geometry'], myCRS, edgecolor='k', facecolor='w')
-
-xmin, ymin, xmax, ymax = outline.total_bounds
 ax.add_feature(outline_feature)  # add the features we've created to the map.
+
+# Add Study Area Box, Display it with a wide outline and no fill colour:
+study_area_outline = ShapelyFeature(study_area['geometry'], myCRS, edgecolor='black', facecolor='none', linewidth=1)
+xmin, ymin, xmax, ymax = study_area.total_bounds
+ax.add_feature(study_area_outline) # add the features we've created to the map.
 
 # using the boundary of the shapefile features, zoom the map to our area of interest
 ax.set_extent([xmin, xmax, ymin, ymax], crs=myCRS)  # because total_bounds gives output as xmin, ymin, xmax, ymax,
@@ -57,7 +62,7 @@ road_class = len(roads.Road_class.unique())  # get the number of unique road cla
 print('Number of unique features: {}'.format(road_class))
 
 # Pick Colours for individual road classes
-road_colours = ['black', 'red', 'gold', 'blue', 'purple', 'green', 'grey', 'cyan']
+road_colours = ['gray', 'red', 'gold', 'blue', 'purple', 'green', 'lime', 'cyan']
 
 # Get a list of unique names for road classes:
 road_class_names = list(roads.Road_class.unique())
@@ -70,25 +75,19 @@ print(road_class_names)
 for i, name in enumerate(road_class_names):
     feat = ShapelyFeature(roads['geometry'][roads['Road_class'] == name], myCRS,
                           edgecolor=road_colours[i],
-                          facecolor=road_colours[i],
+                          facecolor='none',
                           linewidth=1,
                           alpha=0.25)
     ax.add_feature(feat)
+
+plt.show()
 
 """Clips layers to the Study Area.
 
  Each layer must be clipped to the 'Study Area' Shapefile"""
 
-# Add map Layer called Study Area Box:
-study_area = gpd.read_file('data_files/study_area_box.shp')
-study_area_outline = ShapelyFeature(study_area['geometry'], myCRS,#Display it with a wide outline and no fill colour
-                            edgecolor='black',
-                            facecolor='none',
-                            linewidth=2)
-ax.add_feature(study_area_outline)
-plt.show()
 
-# Exercise 2 Clipping Map Layers:
+# Part 3: Clipping Map Layers to the study area:
 def clip_to_study_area(name):
     clipped = gpd.clip(name,study_area)
     clipped_gdf=gpd.Geodataframe(pd.clipped)
@@ -98,23 +97,18 @@ assert isinstance(roads.crs, object)
 print(study_area.crs == roads.crs)
 
 # Clip roads layer to the Study Area Box
-roads_clipped = roads.clip (study_area_outline)
+roads_clipped = roads.clip(study_area_outline)
 ax.add_feature(roads_clipped)
 plt.show()
 
-xmin, ymin, xmax, ymax = study_area_outline.total_bounds
-ax.add_feature(study_area_outline)  # add the features we've created to the map.
 
-# using the boundary of the shapefile features, zoom the map to our area of interest
-ax.set_extent([xmin, xmax, ymin, ymax], crs=myCRS)  # because total_bounds gives output as xmin, ymin, xmax, ymax,
-# but set_extent takes xmin, xmax, ymin, ymax, we re-order the coordinates here.
 
 # Exercise 3 Creating and applying a layer file
 # Exercise 4 Obtaining Map layers online and clipping them to the study area
-# Exercise 5 Symbolising Quantities
+# Exercise 5 Symbolising Quantities (census data)
 # Exercise 6 Creating and displaying point data and saving to a shapefile (Track Centre)
-
 # Exercise 7 Displaying point data and saving to a shapefile (Buildings)
+# Adding and displaying a raster
 # Exercise 14 Create a Map Layout
 
 
